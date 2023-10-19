b24256b935a24be9c92a14261317da0f53ccabed
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-10-18 22:23:14 -0700

    add constants for DataGenFlow, DataGenTextFlow, and TextPlusDataGenFlow

--- uniflow/constants.py
+++ uniflow/constants.py
@@ -1,5 +1,12 @@
 """Flow constants"""
 from uniflow.flow.flow import Flow
+from uniflow.flow.flow_data_gen import DataGenFlow
+from uniflow.flow.flow_data_gen_text import DataGenTextFlow
+from uniflow.flow.flow_text_plus_data_gen import TextPlusDataGenFlow
 
-
-flows_dict = {"flow": Flow}
+flows_dict = {
+    "flow": Flow,
+    "low_data_gen": DataGenFlow,
+    "flow_data_gen_text": DataGenTextFlow,
+    "flow_text_plus_data_gen": TextPlusDataGenFlow,
+}
