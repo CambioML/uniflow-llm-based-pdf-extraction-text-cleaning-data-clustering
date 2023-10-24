dcaecdbefaa96e85e1ce381c76a9127486bc2e00
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-10-24 16:17:31 -0700

    add logging to flow_text_plus_data_gen

--- uniflow/flow/flow_text_plus_data_gen.py
+++ uniflow/flow/flow_text_plus_data_gen.py
@@ -1,10 +1,10 @@
 """ Text Plus Data Gen Flow class."""
+import logging
 from typing import Sequence, Mapping, Any
 from uniflow.node.node import Node
 from uniflow.flow.flow import Flow
 from uniflow.flow.flow_data_gen_text import DataGenTextFlow
 from uniflow.flow.flow_data_gen import DataGenFlow
-import uniflow.flow.constants as constants
 
 
 class TextPlusDataGenFlow(Flow):
@@ -15,6 +15,8 @@ class TextPlusDataGenFlow(Flow):
         super().__init__()
         self._data_gen_text_flow = DataGenTextFlow()
         self._data_gen_flow = DataGenFlow()
+        self._logger = logging.getLogger(__name__)
+        self._logger.setLevel(logging.INFO)
 
     def run(self, nodes: Sequence[Node]) -> Sequence[Node]:
         """Run Text Plus Data Gen Flow.
@@ -26,12 +28,12 @@ class TextPlusDataGenFlow(Flow):
             Sequence of nodes.
         """
         # Run DataTextGen flow
-        print("Starting DataGenTextFlow...")
+        self._logger.info("Starting DataGenTextFlow...")
         data_gen_text_out_nodes = self._data_gen_text_flow.run(nodes)
-        print("DataGenTextFlow complete!")
+        self._logger.info("DataGenTextFlow complete!")
 
         # Run DataGenFlow
-        print("Starting DataGenFlow...")
+        self._logger.info("Starting DataGenFlow...")
         data_gen_out_nodes = self._data_gen_flow.run(data_gen_text_out_nodes)
-        print("DataGenFlow complete!")
+        self._logger.info("DataGenFlow complete!")
         return data_gen_out_nodes
