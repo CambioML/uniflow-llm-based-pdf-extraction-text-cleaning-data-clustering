6578f7baa382e3d99d34645ff37a055ef997bfe5
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-10-27 11:28:37 -0700

    move logger outside of class

--- uniflow/op/self_instructed_op/preprocess_html_op.py
+++ uniflow/op/self_instructed_op/preprocess_html_op.py
@@ -7,15 +7,12 @@ from cleantext import clean
 import uniflow.flow.constants as constants
 from langchain.document_loaders import UnstructuredHTMLLoader
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 
 class PreprocessHTMLOp(LinearOp):
     """Preprocess HTML operation."""
-
-    def __init__(self, name: str):
-        """Initialize PreprocessHTMLOp class."""
-        super().__init__(name)
-        self._logger = logging.getLogger(__name__)
-        self._logger.setLevel(logging.INFO)
 
     def _transform(self, value_dict: Mapping[str, Any]) -> Mapping[str, Any]:
         """Transform value dict.
@@ -26,13 +23,13 @@ class PreprocessHTMLOp(LinearOp):
         Returns:
             Mapping[str, Any]: Output value dict.
         """
-        self._logger.info("Starting Preprocess HTML...")
+        logger.info("Starting Preprocess HTML...")
         html_in = value_dict[constants.HTML_KEY][:]
         loader = UnstructuredHTMLLoader("./" + html_in)
         data = loader.load()
         pages = loader.load_and_split()
-        self._logger.debug("html_in: %s", html_in)
-        self._logger.info("Preprocess HTML Complete!")
-        self._logger.debug("Output %s", pages)
+        logger.debug("html_in: %s", html_in)
+        logger.info("Preprocess HTML Complete!")
+        logger.debug("Output %s", pages)
 
         return {constants.PAGES_KEY: pages}
