e92290fa202a261e644fe1aa43a03b3cfc06cd60
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-11-06 12:33:55 -0800

    add PDFSelfInstructedGenFlow and move initializations to __init__

--- uniflow/flow/flow_self_instructed_gen.py
+++ uniflow/flow/flow_self_instructed_gen.py
@@ -3,12 +3,27 @@ from typing import Sequence, Mapping, Any
 from uniflow.node.node import Node
 from uniflow.flow.flow import Flow
 from uniflow.op.self_instructed_op.preprocess_html_op import PreprocessHTMLOp
+from uniflow.op.self_instructed_op.preprocess_pdf_op import PreprocessPDFOp
 from uniflow.op.self_instructed_op.si_model_inf_op import SIModelInfOp
 from uniflow.op.self_instructed_op.data_output_si_op import DataOutSIOp
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class SelfInstructedGenFlow(Flow):
     """Data generation from HTML via Self Instructed Fine tuning."""
+
+    PreprocessOp = PreprocessHTMLOp
+
+    def __init__(self):
+        """Initialize Self Instructed Gen Flow class."""
+        logger.info("Initializing SelfInstructGenFlow...")
+        super().__init__()
+        self._preprocess_op = self.PreprocessOp("preprocess_op")
+        self._data_output_si_op = DataOutSIOp("data_output_si_op")
+        logger.info("SelfInstructGenFlow initialization complete!")
 
     def run(self, nodes: Sequence[Node]) -> Sequence[Node]:
         """Run flow.
@@ -20,10 +35,14 @@ class SelfInstructedGenFlow(Flow):
             Sequence[Node]: Nodes.
         """
         # Preprocessing for a list of datasets?
-        preprocess_html_op = PreprocessHTMLOp("preprocess_html_op")
-        preprocess_nodes = preprocess_html_op(nodes)
+        preprocess_nodes = self._preprocess_op(nodes)
         si_model_inf_op = SIModelInfOp("si_model_inf_op")
         si_model_nodes = si_model_inf_op(preprocess_nodes)
-        data_output_si_op = DataOutSIOp("data_output_si_op")
-        node_data_out = data_output_si_op(si_model_nodes)
+        node_data_out = self._data_output_si_op(si_model_nodes)
         return node_data_out
+
+
+class PDFSelfInstructedGenFlow(SelfInstructedGenFlow):
+    """Data generation from HTML via Self Instructed Fine tuning."""
+
+    PreprocessOp = PreprocessPDFOp
