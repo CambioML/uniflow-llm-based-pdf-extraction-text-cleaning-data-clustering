3a92693e23d2a63802ae34b7a6eb2477fa053aca
Rachel Hu <goldpiggy@berkeley.edu>
2023-12-19 16:37:13 -0800

    rename uniflow.flow.transform.utils.server to uniflow.flow.transform.utils.model_server

--- uniflow/flow/transform/__init__.py
+++ uniflow/flow/transform/__init__.py
@@ -3,4 +3,4 @@
 # this register all possible model server into ModelServerFactory through
 # ModelServerFactory.register(cls.__name__, cls) in AbsModelServer
 # __init_subclass__
-from uniflow.model.server import *  # noqa: F401, F403
+from uniflow.flow.transform.utils.model_server import *  # noqa: F401, F403
