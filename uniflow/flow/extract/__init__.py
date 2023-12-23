41229ca85e86612996765a209ab8f3978e53da05
sayaZhang <zhangshugar@163.com>
2023-12-23 03:02:49 +0000

    extract markdown

--- uniflow/flow/extract/__init__.py
+++ uniflow/flow/extract/__init__.py
@@ -4,8 +4,8 @@
 # ModelServerFactory.register(cls.__name__, cls) in AbsModelServer
 # __init_subclass__
 
+from uniflow.flow.extract.extract_md_flow import ExtractMarkdownFlow  # noqa: F401;
 from uniflow.flow.extract.extract_pdf_flow import ExtractPDFFlow  # noqa: F401;
-from uniflow.flow.extract.extract_txt_flow import \
-    ExtractTxtFlow  # noqa: F401, F403
+from uniflow.flow.extract.extract_txt_flow import ExtractTxtFlow  # noqa: F401, F403
 
-__all__ = ["ExtractTxtFlow", "ExtractPDFFlow"]
+__all__ = ["ExtractTxtFlow", "ExtractPDFFlow", "ExtractMarkdownFlow"]
