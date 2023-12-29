1a9ee8a20802b671ed28db34bfd7e04fdce45b43
Panzy18 <pandazy18@outlook.com>
2023-12-29 13:29:56 -0800

    refactor rater config

--- uniflow/flow/config.py
+++ uniflow/flow/config.py
@@ -132,6 +132,41 @@ class RaterConfig:
     guided_prompt_template: GuidedPrompt
     num_thread: int = 1
 
+    def __post_init__(self):
+        """Post-initialization to perform label check."""
+        incompatible_labels = self.check_labels()
+        unexprected_labels = incompatible_labels["unexpected_labels"]
+        missing_labels = incompatible_labels["missing_labels"]
+        if unexprected_labels:
+            raise ValueError(
+                "Inconsistent labels found in guided_prompt_template examples, "
+                f"example label {unexprected_labels} not in label2score has keys {list(self.label2score.keys())}",
+            )
+        if missing_labels:
+            print(f"The label2score label {missing_labels} not in example label.")
+
+    def check_labels(self) -> Dict[str, list]:
+        """
+        Check if every label in the guided_prompt_template's examples is a key in label2score.
+
+        Returns:
+            Dict: Incompatible Keys, fields:
+                - 'missing_labels' (List[str]): labels in label2score but not in examples, this may cause performance lose.
+                - 'unxpected_labels' (List[str]): labels in examples but not in label2score, this cause ValueError.
+        """
+        # TODO: Does label strictly match in upper/lower letter?
+        example_labels = set()
+        for example in self.guided_prompt_template.examples:
+            example_labels.add(example.label)
+        label2score_labels = set(self.label2score.keys())
+
+        missing_labels = label2score_labels - example_labels
+        unexpected_labels = example_labels - label2score_labels
+        return {
+            "missing_labels": list(missing_labels),
+            "unexpected_labels": list(unexpected_labels),
+        }
+
 
 @dataclass
 class RaterClassificationConfig(RaterConfig):
@@ -164,27 +199,6 @@ class RaterClassificationConfig(RaterConfig):
         ],
     )
 
-    def __post_init__(self):
-        """Post-initialization to perform label check."""
-        for example in self.guided_prompt_template.examples:
-            if example.label.lower() not in [k.lower() for k in self.label2score]:
-                raise ValueError(
-                    "Inconsistent labels found in guided_prompt_template examples, "
-                    f"example label {example.label} not in label2score has keys {list(self.label2score.keys())}",
-                )
-
-    def check_labels_in_label2score(self) -> bool:
-        """
-        Check if every label in the guided_prompt_template's examples is a key in label2score.
-
-        Returns:
-            bool: True if all labels are in label2score, False otherwise.
-        """
-        for example in self.guided_prompt_template.examples:
-            if example.label not in self.label2score:
-                return False
-        return True
-
 
 @dataclass
 class RaterForGeneratedAnswerConfig(RaterConfig):
@@ -209,18 +223,18 @@ class RaterForGeneratedAnswerConfig(RaterConfig):
         The response should not include examples in the prompt.""",
         examples=[
             Context(
-                context="Basic operating system features were developed in the 1950s, such as resident monitor functions that could automatically run different programs in succession to speed up processing.",
+                context="Basic operating system features were developed in the 1950s, and more complex functions were introduced in the 1960s.",
                 question="When were basic operating system features developed?",
-                grounding_answer="Basic operating system features were developed in the 1980s.",
-                generated_anser="Basic operating system features were developed in the 1950s",
+                grounding_answer="In the 1960s, people developed some basic operating system functions.",
+                generated_answer="Basic operating system features were developed in the 1950s.",
                 explanation="The generated answer is much better because it correctly identifies the 1950s as the time when basic operating system features were developed",
                 label="Strong accept",
             ),
             Context(
-                context="Early computers were built to perform a series of single tasks, like calculating. Basic operating system could automatically run different programs in succession to speed up processing.",
+                context="Early computers were built to perform a series of single tasks, like a calculator. Basic operating system could automatically run different programs in succession to speed up processing.",
                 question="Did early computers function like modern calculators?",
-                grounding_answer="No. Early computers were used primarily for complex calculating like solving ODE.",
-                generated_anser="Yes. Early computers were built to perform a series of single tasks, similar to a calculator.",
+                grounding_answer="No. Early computers were used primarily for complex calculating.",
+                generated_answer="Yes. Early computers were built to perform a series of single tasks, similar to a calculator.",
                 explanation="The generated answer is better as it correctly captures the essence of the early computers' functionality, which was to perform single tasks akin to calculators.",
                 label="Accept",
             ),
@@ -228,7 +242,7 @@ class RaterForGeneratedAnswerConfig(RaterConfig):
                 context="Operating systems did not exist in their modern and more complex forms until the early 1960s. Hardware features were added, that enabled use of runtime libraries, interrupts, and parallel processing.",
                 question="When did operating systems start to resemble their modern forms?",
                 grounding_answer="Operating systems started to resemble their modern forms in the early 1960s.",
-                generated_anser="Modern and more complex forms of operating systems began to emerge in the early 1960s.",
+                generated_answer="Modern and more complex forms of operating systems began to emerge in the early 1960s.",
                 explanation="Both answers are equally good as they accurately pinpoint the early 1960s as the period when modern operating systems began to develop.",
                 label="Equivalent",
             ),
@@ -236,41 +250,20 @@ class RaterForGeneratedAnswerConfig(RaterConfig):
                 context="Operating systems did not exist in their modern and more complex forms until the early 1960s. Hardware features were added, that enabled use of runtime libraries, interrupts, and parallel processing.",
                 question="What features were added to hardware in the 1960s?",
                 grounding_answer="Hardware in the 1960s saw the addition of features like runtime libraries and parallel processing.",
-                generated_anser="The 1960s saw the addition of fault tolerance and compatibility in hardware.",
+                generated_answer="The 1960s saw the addition of input output control and compatible timesharing capabilities in hardware.",
                 explanation="The generated answer is worse because it inaccurately suggests the addition of capabilities of hardware in 1960s which is not supported by the context.",
                 label="Reject",
             ),
             Context(
                 context="Operating systems did not exist in their modern and more complex forms until the early 1960s. When personal computers became popular in the 1980s, operating systems were made for them similar in concept to those used on larger computers.",
                 question="When did operating systems in personal computer were similar to those used on larger computers?",
-                grounding_answer="In 1980s.",
-                generated_anser="In the early 1960s.",
+                grounding_answer="In 1980s, as personal computers became popular.",
+                generated_answer="In the early 1960s, as operating system became more complex.",
                 explanation="The generated answer is much worse as it incorrectly states the early 1960s as the period of popularity for personal computers, contradicting the context which indicates the 1980s.",
                 label="Strong reject",
             ),
         ],
     )
-
-    def __post_init__(self):
-        """Post-initialization to perform label check."""
-        for example in self.guided_prompt_template.examples:
-            if example.label.lower() not in [k.lower() for k in self.label2score]:
-                raise ValueError(
-                    "Inconsistent labels found in guided_prompt_template examples, "
-                    f"example label {example.label} not in label2score has keys {list(self.label2score.keys())}",
-                )
-
-    def check_labels_in_label2score(self) -> bool:
-        """
-        Check if every label in the guided_prompt_template's examples is a key in label2score.
-
-        Returns:
-            bool: True if all labels are in label2score, False otherwise.
-        """
-        for example in self.guided_prompt_template.examples:
-            if example.label not in self.label2score:
-                return False
-        return True
 
 
 ###########################################################
