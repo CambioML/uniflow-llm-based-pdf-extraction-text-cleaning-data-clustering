aa20bb2b5a8ac0294efc95dec46cce6f172c1df4
panzy <zeyupan@outlook.com>
2023-12-28 19:11:19 -0800

    add rater for generated answer

--- uniflow/flow/config.py
+++ uniflow/flow/config.py
@@ -186,6 +186,93 @@ class RaterClassificationConfig(RaterConfig):
         return True
 
 
+@dataclass
+class RaterForGeneratedAnswerConfig(RaterConfig):
+    """Rater classification Config Class."""
+
+    flow_name: str = "RaterForGeneratedAnswerFlow"
+    model_config: ModelConfig = OpenAIModelConfig()
+    label2score: Dict[str, float] = field(
+        default_factory=lambda: {
+            "Strong accept": 2.0,
+            "Accept": 1.0,
+            "Equivalent": 0.0,
+            "Reject": -1.0,
+            "Strong reject": -2.0,
+        }
+    )
+    # NOTE: This flow seems very sensitive to the choice of prompt.
+    # For a more stable performance, prompt should be improved.
+    guided_prompt_template: GuidedPrompt = GuidedPrompt(
+        instruction="""Rate the generated answer compared to the grounding answer to the question. Accept means the generated answer is better than the grounding answer and reject means worse.
+        Follow the format of the examples below to include context, question, grounding answer, generated answer and label in the response.
+        The response should not include examples in the prompt.""",
+        examples=[
+            Context(
+                context="Basic operating system features were developed in the 1950s, such as resident monitor functions that could automatically run different programs in succession to speed up processing.",
+                question="When were basic operating system features developed?",
+                grounding_answer="Basic operating system features were developed in the 1980s.",
+                generated_anser="Basic operating system features were developed in the 1950s",
+                explanation="The generated answer is much better because it correctly identifies the 1950s as the time when basic operating system features were developed",
+                label="Strong accept",
+            ),
+            Context(
+                context="Early computers were built to perform a series of single tasks, like calculating. Basic operating system could automatically run different programs in succession to speed up processing.",
+                question="Did early computers function like modern calculators?",
+                grounding_answer="No. Early computers were used primarily for complex calculating like solving ODE.",
+                generated_anser="Yes. Early computers were built to perform a series of single tasks, similar to a calculator.",
+                explanation="The generated answer is better as it correctly captures the essence of the early computers' functionality, which was to perform single tasks akin to calculators.",
+                label="Accept",
+            ),
+            Context(
+                context="Operating systems did not exist in their modern and more complex forms until the early 1960s. Hardware features were added, that enabled use of runtime libraries, interrupts, and parallel processing.",
+                question="When did operating systems start to resemble their modern forms?",
+                grounding_answer="Operating systems started to resemble their modern forms in the early 1960s.",
+                generated_anser="Modern and more complex forms of operating systems began to emerge in the early 1960s.",
+                explanation="Both answers are equally good as they accurately pinpoint the early 1960s as the period when modern operating systems began to develop.",
+                label="Equivalent",
+            ),
+            Context(
+                context="Operating systems did not exist in their modern and more complex forms until the early 1960s. Hardware features were added, that enabled use of runtime libraries, interrupts, and parallel processing.",
+                question="What features were added to hardware in the 1960s?",
+                grounding_answer="Hardware in the 1960s saw the addition of features like runtime libraries and parallel processing.",
+                generated_anser="The 1960s saw the addition of fault tolerance and compatibility in hardware.",
+                explanation="The generated answer is worse because it inaccurately suggests the addition of capabilities of hardware in 1960s which is not supported by the context.",
+                label="Reject",
+            ),
+            Context(
+                context="Operating systems did not exist in their modern and more complex forms until the early 1960s. When personal computers became popular in the 1980s, operating systems were made for them similar in concept to those used on larger computers.",
+                question="When did operating systems in personal computer were similar to those used on larger computers?",
+                grounding_answer="In 1980s.",
+                generated_anser="In the early 1960s.",
+                explanation="The generated answer is much worse as it incorrectly states the early 1960s as the period of popularity for personal computers, contradicting the context which indicates the 1980s.",
+                label="Strong reject",
+            ),
+        ],
+    )
+
+    def __post_init__(self):
+        """Post-initialization to perform label check."""
+        for example in self.guided_prompt_template.examples:
+            if example.label.lower() not in [k.lower() for k in self.label2score]:
+                raise ValueError(
+                    "Inconsistent labels found in guided_prompt_template examples, "
+                    f"example label {example.label} not in label2score has keys {list(self.label2score.keys())}",
+                )
+
+    def check_labels_in_label2score(self) -> bool:
+        """
+        Check if every label in the guided_prompt_template's examples is a key in label2score.
+
+        Returns:
+            bool: True if all labels are in label2score, False otherwise.
+        """
+        for example in self.guided_prompt_template.examples:
+            if example.label not in self.label2score:
+                return False
+        return True
+
+
 ###########################################################
 #                    Pipeline Config                      #
 ###########################################################
