3532841e8871a4f06bf78553ca3c10d9042a94f3
Rachel Hu <goldpiggy@berkeley.edu>
2024-01-12 01:20:01 -0800

    Bump default HuggingfaceModelConfig to use Mistral-7B-Instruct-v0.2 from v0.1

--- uniflow/op/model/model_config.py
+++ uniflow/op/model/model_config.py
@@ -40,7 +40,7 @@ class AzureOpenAIModelConfig:
 class HuggingfaceModelConfig(ModelConfig):
     """Huggingface Model Config Class."""
 
-    model_name: str = "mistralai/Mistral-7B-Instruct-v0.1"
+    model_name: str = "mistralai/Mistral-7B-Instruct-v0.2"
     batch_size: int = 1
     model_server: str = "HuggingfaceModelServer"
     neuron: bool = False
