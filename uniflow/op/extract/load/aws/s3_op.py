0fbb19f3efcd4626827c87964b280c0aaf426b38
Rachel Hu <goldpiggy@berkeley.edu>
2024-01-18 09:02:02 -0800

    Add aws integration for uniflow

---
+++ uniflow/op/extract/load/aws/s3_op.py
@@ -0,0 +1,57 @@
+"""S3 Op Class."""
+
+import copy
+import os
+from typing import Sequence
+
+import boto3
+
+from uniflow.node import Node
+from uniflow.op.op import Op
+
+
+class ExtractS3Op(Op):
+    """Op to download a file from s3."""
+
+    LOCAL_FILE_PATH = "/tmp/aws/s3"
+
+    def __init__(self, name: str = "extract_s3_op") -> None:
+        self._s3_client = boto3.client("s3")
+        super().__init__(name=name)
+
+    def __call__(self, nodes: Sequence[Node]) -> Sequence[Node]:
+        """Run Model Op.
+
+        Args:
+            nodes (Sequence[Node]): Nodes to run.
+
+        Returns:
+            Sequence[Node]: Nodes after running.
+        """
+        output_nodes = []
+        for node in nodes:
+            value_dict = copy.deepcopy(node.value_dict)
+            # create local file path if not exists
+            if os.path.exists(self.LOCAL_FILE_PATH) is False:
+                os.makedirs(self.LOCAL_FILE_PATH)
+            filename = os.path.join(self.LOCAL_FILE_PATH, value_dict["key"])
+            print(f"Downloading {value_dict['key']} to {filename}")
+            self._s3_client.download_file(
+                Bucket=value_dict["bucket"],
+                Key=value_dict["key"],
+                Filename=filename,
+            )
+            with open(
+                filename,
+                "r",
+                encoding=value_dict.get("encoding", "utf-8"),
+            ) as f:
+                text = f.read()
+            output_nodes.append(
+                Node(
+                    name=self.unique_name(),
+                    value_dict={"text": text},
+                    prev_nodes=[node],
+                )
+            )
+        return output_nodes
