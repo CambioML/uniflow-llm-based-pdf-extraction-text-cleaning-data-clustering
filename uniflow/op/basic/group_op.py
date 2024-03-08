d2a8ea02132393cedc94ed8370449b60ffb88858
Yifan Zhang <yifan-zhang@berkeley.edu>
2024-03-07 22:32:30 -0800

    style

--- uniflow/op/basic/group_op.py
+++ uniflow/op/basic/group_op.py
@@ -8,7 +8,7 @@ from uniflow.op.prompt import Context
 
 
 class GroupOp(Op):
-    """Customizable Operation."""
+    """Group Operation."""
 
     def __init__(
         self,
@@ -16,51 +16,33 @@ class GroupOp(Op):
         preprocss_fn: Callable[[Mapping[str, Any], Mapping[str, Any]], Mapping[str, Any]],
         fn: Callable[[Mapping[str, Any], Mapping[str, Any]], Mapping[str, Any]],
     ) -> None:
-        """Initializes reduce operation.
+        """Initializes group operation.
 
         Args:
-            name (str): Name of the reduce operation.
-            fn (callable): Function to reduce.
+            name (str): Name of the group operation.
+            preprocss_fn (callable): Function to extract.
+            fn (callable): Function to group.
         """
         super().__init__(name)
         self._fn = fn
         self._preprocess_fn = preprocss_fn
 
     def __call__(self, nodes_1: Sequence[Node], nodes_2: Sequence[Node]) -> Sequence[Node]:
-        """Calls reduce operation.
+        """Calls group operation.
 
         Args:
-            nodes_1, nodes_2: Input two nodes list.
+            nodes_1 (Sequence[Node]), nodes_2 (Sequence[Node]): Input two nodes.
 
         Returns:
             Sequence[Node]: Output nodes.
         """
-
-        # examples
-        """
-        node_a0 = Node("node_a0", ['Introduction'])
-        node_a1 = Node("node_a1", ['Introduction'])
-        node_a2 = Node("node_a2", ['Abstract'])
-
-        node_b0 = Node("node_b0", ['A paper about life itself'])
-        node_b1 = Node("node_b1", ['Life is complicated'])
-        node_b2 = Node("node_b2", ['Happy wife, happy life'])
-
-        nodes_1 = [node_a0, node_a1, node_a2]
-        nodes_2 = [node_b0, node_b1, node_b2]
-        """
-
         output_nodes = []
 
-        # TODO: handling for missing or uneven sections?
         labels, summaries = zip(*self._preprocess_fn(nodes_1, nodes_2))
         aggregated_summaries = self._fn(labels, summaries)
 
-        # Sorted to make sure 1:1 match for later comparison
         sorted_labels = sorted(aggregated_summaries.keys())
-        print("sorted_labels: ", sorted_labels)
 
-        # Get prev nodes
         label_nodes = {label: [] for label in sorted_labels}
 
         for node in nodes_1:
